76d153cbb072f403a800c4412d91b820549c6c8c
Russell Brown <rjbrown57@gmail.com>
2025-01-12 14:21:50 -0600

    feat: add tests to verify supplying directory for config is behaving as expected

--- pkg/types/config/config_test.go
+++ pkg/types/config/config_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -10,10 +11,23 @@ import (
 
 func TestNewCartographerConfig(t *testing.T) {
 
+	// Prep the single file test
 	config, err := utils.WriteTestConfig()
 	if err != nil {
 		t.Fatalf("Failed to write test config %s", err)
 	}
+
+	// Prep the directory test
+	dir, err := utils.WriteTestDir()
+	if err != nil {
+		t.Fatalf("Failed to write test dir %s", err)
+	}
+
+	t.Cleanup(func() {
+		config.Close()
+		os.Remove(config.Name())
+		os.RemoveAll(dir)
+	})
 
 	controlConfig := CartographerConfig{}
 
@@ -22,15 +36,41 @@ func TestNewCartographerConfig(t *testing.T) {
 		t.Fatalf("Failed to unmarshal control config %s", err)
 	}
 
-	t.Cleanup(func() {
-		config.Close()
-		os.Remove(config.Name())
-	})
-
+	// Test a single file
 	c := NewCartographerConfig(config.Name())
 
 	utils.AssertDeepEqual(t, c.ApiVersion, controlConfig.ApiVersion)
 	utils.AssertDeepEqual(t, c.ServerConfig, controlConfig.ServerConfig)
 	utils.AssertDeepEqual(t, c.Groups, controlConfig.Groups)
 	utils.AssertDeepEqual(t, c.Links, controlConfig.Links)
+
+	controlConfig = CartographerConfig{}
+
+	err = yaml.Unmarshal([]byte(fmt.Sprintf("%s\n%s\n%s", utils.TestFullConfig, utils.LinkOnly1Config, utils.LinkOnly2Config)), &controlConfig)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal control config %s", err)
+	}
+
+	// Test a directory
+	c = NewCartographerConfig(dir)
+
+	utils.AssertDeepEqual(t, c.ApiVersion, controlConfig.ApiVersion)
+	utils.AssertDeepEqual(t, c.ServerConfig, controlConfig.ServerConfig)
+	utils.AssertDeepEqual(t, c.Groups, controlConfig.Groups)
+	// This can have issue with the ordering
+	// So i'm being lazy and cheating here
+	utils.AssertDeepEqual(t, len(c.Links), len(controlConfig.Links))
+}
+func TestSetApi(t *testing.T) {
+	c := CartographerConfig{}
+
+	// Test when ApiVersion is not set
+	c.SetApi()
+	utils.AssertDeepEqual(t, c.ApiVersion, ApiVersion)
+
+	// Test when ApiVersion is already set
+	customApiVersion := "v2alpha"
+	c.ApiVersion = customApiVersion
+	c.SetApi()
+	utils.AssertDeepEqual(t, c.ApiVersion, customApiVersion)
 }
