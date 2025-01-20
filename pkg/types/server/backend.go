ec592d9f138cfc60643f2dc1aaa3edb9ce992a7f
Russell Brown <rjbrown57@gmail.com>
2025-01-20 15:35:04 -0600

    refactor: update backend interface for new proto definitions

--- pkg/types/server/backend.go
+++ pkg/types/server/backend.go
@@ -7,19 +7,19 @@ import (
 	"google.golang.org/grpc"
 )
 
-func (c *CartographerServer) Add(_ context.Context, in *proto.CartographerRequest) (*proto.CartographerResponse, error) {
+func (c *CartographerServer) Add(_ context.Context, in *proto.CartographerAddRequest) (*proto.CartographerAddResponse, error) {
 	return c.Backend.Add(in)
 }
 
-func (c *CartographerServer) Get(_ context.Context, in *proto.CartographerRequest) (*proto.CartographerResponse, error) {
+func (c *CartographerServer) Get(_ context.Context, in *proto.CartographerGetRequest) (*proto.CartographerGetResponse, error) {
 	return c.Backend.Get(in)
 }
 
-func (c *CartographerServer) Delete(_ context.Context, in *proto.CartographerRequest) (*proto.CartographerResponse, error) {
+func (c *CartographerServer) Delete(_ context.Context, in *proto.CartographerDeleteRequest) (*proto.CartographerDeleteResponse, error) {
 	return c.Backend.Delete(in)
 }
 
-func (c *CartographerServer) StreamGet(in *proto.CartographerRequest, stream grpc.ServerStreamingServer[proto.CartographerResponse]) error {
+func (c *CartographerServer) StreamGet(in *proto.CartographerStreamGetRequest, stream grpc.ServerStreamingServer[proto.CartographerStreamGetResponse]) error {
 	// https://grpc.io/docs/languages/go/basics/#server-side-streaming-rpc
 	return c.Backend.StreamGet(in, stream)
 }
