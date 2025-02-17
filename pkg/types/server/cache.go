cf64f4af355d08a08b6524eaebbcdac4ba8378df
Russell Brown <rjbrown57@gmail.com>
2025-02-17 09:29:47 -0600

    feat: add locking

--- pkg/types/server/cache.go
+++ pkg/types/server/cache.go
@@ -7,6 +7,7 @@ import (
 )
 
 func (c *CartographerServer) AddToCache(v interface{}) {
+	c.mu.Lock()
 	switch v.(type) {
 	case *proto.Link:
 		l := v.(*proto.Link)
@@ -21,11 +22,14 @@ func (c *CartographerServer) AddToCache(v interface{}) {
 		c.cache[g.Name] = g
 		c.groupCache[g.Name] = g
 	}
+	c.mu.Unlock()
 }
 
 func (c *CartographerServer) DeleteFromCache(key ...string) {
+	c.mu.Lock()
 	log.Debugf("Deleting %s from cache", key)
 	for _, k := range key {
 		delete(c.cache, k)
 	}
+	c.mu.Unlock()
 }
