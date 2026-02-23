b85d8e5eedc05279c81f88edeccdc8d2d147a5af
Russell Brown <rjbrown57@gmail.com>
2026-02-22 19:58:41 -0500

    refactor: update add,delete to use ns normalization

--- pkg/types/server/delete.go
+++ pkg/types/server/delete.go
@@ -11,16 +11,21 @@ import (
 
 func (c *CartographerServer) Delete(_ context.Context, in *proto.CartographerDeleteRequest) (*proto.CartographerDeleteResponse, error) {
 
+	var err error
+
 	// record the duration of the delete operation
 	defer metrics.RecordOperationDuration("delete")()
 
+	ns, err := proto.GetNamespace(in.GetNamespace())
+	if err != nil {
+		return nil, err
+	}
+
 	// delete from cache
-	c.DeleteFromCache(in.GetNamespace(), in.Ids...)
+	c.DeleteFromCache(ns, in.Ids...)
 
 	// delete from backend
 	r := c.Backend.Delete(in)
-
-	var err error
 
 	if len(r.Errors) > 0 {
 		err = fmt.Errorf("error deleting keys: %s", r.Errors)
